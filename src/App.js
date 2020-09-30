46f9d415922dc4ec67a5db6f135e5018ad3cd708
Huzaifa Zahoor <57295548+huzaifazahoorchohan@users.noreply.github.com>
2020-10-01 02:31:41 +0500

    Case Boxes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 // Components
 import Header from './Components/Header';
+import CasesBox from './Components/CasesBox';
 
 const App = () => {
   return(
     <div>
       <Header />
+      <CasesBox />
     </div>
   )
 };
