a96979520cb2c0b3acacf781f4f681f85393d6bb
Huzaifa Zahoor <57295548+huzaifazahoorchohan@users.noreply.github.com>
2020-10-02 21:23:11 +0500

    Erase components
    Reset plate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import './App.css';
-
-// Components
-import Header from './Components/Header';
-import CasesBox from './Components/CasesBox';
-import Charts from './Components/Charts';
 
 const App = () => {
   return(
     <div>
-      <Header />
-      <CasesBox />
-      <Charts />
+      
     </div>
   )
 };
