807c3fbd7aba2bb51e5374b1d0017e27189ec0fe
Huzaifa Zahoor <57295548+huzaifazahoorchohan@users.noreply.github.com>
2020-10-02 21:54:22 +0500

    New simple header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './App.css';
+
+// Bootstrap
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+// Components
+import Header from './Components/Header';
 
 const App = () => {
   return(
     <div>
-      
+        <Header />
     </div>
   )
 };
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div className = 'container-fluid' style = {{backgroundColor:'#16A085',boxShadow:'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
+            <h1 style = {{color:'whitesmoke',lineHeight:'70px',fontSize:'25px'}} >COVID-19 | TRACKER</h1>
+        </div>
+    )
+};
+
+export default Header;
