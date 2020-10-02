25e50bd21aeb05d380e62e28f37a44e679436cc7
Huzaifa Zahoor <57295548+huzaifazahoorchohan@users.noreply.github.com>
2020-10-02 23:45:02 +0500

    Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div className = 'container-fluid header'>
+            <div>
+                <h4>COVID-19</h4>
+                <h2>TRACKER</h2>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
